cd7fda09e41763b531665bdcd8cc4b399dac52f8
Boris Rose <boris.alexandre.rose@gmail.com>
2024-06-14 16:37:54 +0200

    feat/login.html

---
+++ src/js/constants/forbiddenWords.js
@@ -0,0 +1,11 @@
+export const forbiddenWords = [
+    '<script>',
+    '</script>',
+    '<html>',
+    '</html>',
+    'sapristi',
+    'petit bonhomme à pédale wawa',
+    'sacrebleu',
+    'tonnerre de Brest'
+    
+]
