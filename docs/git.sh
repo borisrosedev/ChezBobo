5fd7ade112d0564e17755cc000532338c36769e1
Boris Rose <boris.alexandre.rose@gmail.com>
2024-06-18 09:30:32 +0200

    feat/docs

---
+++ docs/git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+git clone #permet de cloner un dépôt distant existant 
+
+git push #permet d'envoyer vers Internet un commit effectué en local 
+
+git add ... #permet d'indexer une modification et la rendre commitable 
+
+git commit -m"feat/fix" #permet de sauvegarder une version de votre dépôt local
+
+git status #permet de vérifier l'état du dépôt local
+
+git pull #permet de rapatrier sur son dépôt des modifications qui se trouvent sur le dépôt distant
